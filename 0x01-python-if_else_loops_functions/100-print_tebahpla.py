119156b4c97cdfa15f64d4982260c0e927772c32
Pato <99479428+patrickateng@users.noreply.github.com>
2022-06-01 00:14:40 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end="")
+i = 122
+while i >= 97:
+    flag = 0
+    if i % 2 != 0:
+        i = i - 32
+        flag = 1
+    print("{:s}".format(chr(i)), end="")
+    if flag == 1:
+        i = i + 32
+    i = i - 1
