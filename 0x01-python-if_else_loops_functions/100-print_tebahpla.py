df575a343057d3b16350a5d5e86d5b677edac047
Pato <99479428+patrickateng@users.noreply.github.com>
2022-05-31 13:08:14 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(c, chr(c - 33)), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format(i - 32) if i % 2 != 0 else "{:c}".format(i), end="")
