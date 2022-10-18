e15c3463f5d6d24a1a30a11ead5b33a310e9ea51
Pato <99479428+patrickateng@users.noreply.github.com>
2022-10-18 15:52:08 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-  /* const nb = JSON.parse(body).results.reduce((acc, elem) => {
-    acc += elem.characters.reduce((acc, character) => {
-      return (character === 'https://swapi.co/api/people/18/' ? acc + 1 : acc);
-    }, 0);
-    return (acc);
-  }, 0);
-  */
-  const nb = JSON.parse(body).results.filter((elem) => {
-    return elem.characters.filter((url) => { return url.includes('18'); }).length;
-  }).length;
-  console.log(nb);
 });
